116fca77e4bcc2c5b3d89e77f977ac09ffddb1ee
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-04-11 12:03:15 +0530

    readme

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,23 @@
 // write the component code here
+// write the component code here
+import React from 'react'
+
+const usercard = () => {
+
+    const profilePhoto = "https://i.pinimg.com/474x/04/c2/a6/04c2a61d6837d23044da021ba7a13bcb.jpg"; 
+    const name = "Rohitha";
+    const email = "rohitha22@gmail.com" ;
+    const phoneNumber = "9494578519";
+    const address = "Rajamundry";
+  return (
+    <div>
+      <img src={profilePhoto} alt="Image" />
+      <p>{name}</p>
+      <p>{email}</p>
+      <p>{phoneNumber}</p>
+      <p>{address}</p>
+    </div>
+  )
+}
+
+export default usercard
